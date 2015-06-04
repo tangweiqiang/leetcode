e061bc5eaee5aad2363992d095ee5261c06cbf73
tangweiqiang <ndsxiao@gmail.com>
2015-06-04 17:45:17 +0800

    Merge branch 'master' of https://github.com/tangweiqiang/leetcode
    Conflicts:
    	src/java/Solution.java

--- src/javasolution/Solution006.java
+++ src/javasolution/Solution006.java
@@ -5,22 +5,22 @@ package javasolution;
  */
 public class Solution006 {
     public String convert(String s, int numRows) {
-        if (numRows <= 1)
+        if (numRows <= 1 || s.length()<=numRows)
             return s;
         String result="";
         int step = 2 * numRows - 2;
 
-        int n = (s.length()/step==0)?s.length()/step:s.length()/step + 1;
+        int n = (s.length()%step==0)?s.length()/step:s.length()/step + 1;
         for (int i=0;i<s.length();i+=step)
             result += s.charAt(i) ;
-        for (int i=1;i<numRows;i++){
+        for (int i=1;i<numRows-1;i++){
             for (int j=0;j<n-1;j++){
                 result = result + s.charAt(step*j+i) + s.charAt((j+1)*step-i);
             }
-            if (step*n+i < s.length())
-                result += s.charAt(step*n+i);
-            if ((n+1)*step-i < s.length())
-                result += s.charAt((n+1)*step-i);
+            if (step*(n-1)+i < s.length())
+                result += s.charAt(step*(n-1)+i);
+            if (n*step-i < s.length())
+                result += s.charAt(n*step-i);
         }
         for (int i=numRows-1;i<s.length();i+=step)
             result += s.charAt(i);
@@ -28,6 +28,6 @@ public class Solution006 {
     }
 
     public static void main(String[] args) {
-        System.out.println(new Solution006().convert("PAYPALISHIRING",3));
+        System.out.println(new Solution006().convert("ABCDE",4));
     }
 }
