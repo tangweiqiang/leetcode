7f260b096721fed1e36dab8d06593fd0733e40e8
tangweiqiang <ndsxiao@gmail.com>
2015-06-04 18:08:51 +0800

    Merge branch 'master' of https://github.com/tangweiqiang/leetcode
    Conflicts:
    	src/java/Solution.java

---
+++ src/javasolution/Sulution012.java
@@ -0,0 +1,36 @@
+package javasolution;
+
+/**
+ * Created by Administrator on 2015/6/4.
+ */
+public class Sulution012 {
+    public String intToRoman(int num) {
+        int[] val = {
+                1000, 900, 500, 400,
+                100, 90, 50, 40,
+                10, 9, 5, 4,
+                1
+        };
+        String[] syb = new String[] {
+                "M", "CM", "D", "CD",
+                "C", "XC", "L", "XL",
+                "X", "IX", "V", "IV",
+                "I"
+        };
+        StringBuffer roman = new StringBuffer();
+        int i=0 ,k;
+        while (num>0){
+            k = num /val[i];
+            while (k-- >0) {
+                roman.append(syb[i]);
+                num -= val[i];
+            }
+            i++;
+        }
+        return roman.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Sulution012().intToRoman(3000));
+    }
+}
