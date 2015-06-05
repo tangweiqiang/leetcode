c80bc617528bee6ed30e216f977b83968388bf72
tangweiqiang <ndsxiao@gmail.com>
2015-06-05 17:10:49 +0800

    Merge branch 'master' of https://github.com/tangweiqiang/leetcode
    Conflicts:
    	src/java/Solution.java

---
+++ src/javasolution/Solution028.java
@@ -0,0 +1,11 @@
+package javasolution;
+
+/**
+ * Created by 汤伟强 on 2015/6/5.
+ */
+public class Solution028 {
+    public int strStr(String haystack, String needle) {
+
+        return haystack.indexOf(needle);
+    }
+}
---
+++ src/javasolution/Solution050.java
@@ -0,0 +1,10 @@
+package javasolution;
+
+/**
+ * Created by 汤伟强 on 2015/6/5.
+ */
+public class Solution050 {
+    public double myPow(double x, int n) {
+        return 0;
+    }
+}
